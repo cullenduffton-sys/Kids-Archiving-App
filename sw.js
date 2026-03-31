67706b9cb1b50de7b487e9d5c14a403e49703dc5
cullenduffton-sys <cullen.duffton@gmail.com>
2026-03-31 13:57:11 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-self.addEventListener('fetch', function(event) {});
+const version = "1.0.1";
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+    // Dummy fetch to satisfy PWA requirements
+});
